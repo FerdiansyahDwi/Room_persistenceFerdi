f15fd0aec9558e62660a8ed979e93a81726b08bb
FerdiansyahDwi <ferdiansyahdwi00@gmail.com>
2024-12-20 13:17:19 +0700

    update delete room persistence

--- Room_persistence/app/src/main/java/com/example/room_persistence/MainActivity.kt
+++ Room_persistence/app/src/main/java/com/example/room_persistence/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -19,6 +20,7 @@ import com.example.room_persistence.room.MainViewModel
 import com.example.room_persistence.room.Note
 import com.example.room_persistence.room.NoteAdapter
 import com.example.room_persistence.ui.AddNoteActivity
+import com.example.room_persistence.ui.EditNoteActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
@@ -27,8 +29,19 @@ class MainActivity : AppCompatActivity() {
 
     private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
-            Toast.makeText(this, "Note Add Successfully", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Data berhasil ditambahkan", Toast.LENGTH_SHORT).show()
             mainViewModel.fetchNotes()
+        }
+    }
+
+    private val startForResultEdit = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val updatedNote = result.data?.getSerializableExtra("updatedNote") as Note
+            updatedNote.let {
+                Toast.makeText(this, "Data berhasil di edit", Toast.LENGTH_SHORT).show()
+                mainViewModel.fetchNotes()
+            }
+
         }
     }
 
@@ -46,7 +59,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         
-        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         mainViewModel.allNotes.observe(this){notes ->
             updateRecyclerView(notes)
@@ -60,13 +73,44 @@ class MainActivity : AppCompatActivity() {
         }
 
         private fun updateRecyclerView(notes: List<Note>) {
-            if (::noteAdapter.isInitialized) {
-                noteAdapter.notifyDataSetChanged()
-                noteAdapter = NoteAdapter(notes)
-                recyclerView.adapter = noteAdapter
-            } else {
-                noteAdapter = NoteAdapter(notes)
-                recyclerView.adapter = noteAdapter
-            }
+            noteAdapter = NoteAdapter(notes,
+                onDeleteListener = {noteId -> showDeleteConfirmationDialog(noteId)},
+                onEditListener = {note -> showEditConfirmationDialog(note)})
+            recyclerView.adapter = noteAdapter
+            noteAdapter.notifyDataSetChanged()
         }
-    }
+
+        private fun showEditConfirmationDialog(note: Note) {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Edit Note")
+                .setMessage("Do you want to edit this note?")
+                .setPositiveButton("Yes") {dialog, _ ->
+                    val intent = Intent(this, EditNoteActivity::class.java)
+                    intent.putExtra("note", note)
+                    startForResultEdit.launch(intent)
+                    dialog.dismiss()
+                }
+                .setNegativeButton("No") {
+                    dialog, _ ->
+                    dialog.dismiss()
+                }
+            builder.create().show()
+        }
+
+        private fun showDeleteConfirmationDialog(noteId: Int) {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Delete note")
+                .setMessage("Are you sure you want to delete this note")
+                .setPositiveButton("Yes") {dialog, _ ->
+                    mainViewModel.deleteNoteById(noteId)
+                    Toast.makeText(this, "Note delete successfully", Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                    mainViewModel.fetchNotes()
+                }
+                .setNegativeButton("No") {
+                    dialog, _ ->
+                    dialog.dismiss()
+                }
+            builder.create().show()
+        }
+}
